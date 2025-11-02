a020047353b62ebe733665fa89b5b5d705203bf6
Name <example@gmail.com>
2025-11-02 14:10:42 +0700

    onnne deploy232

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 
 // Импорт компонентов
